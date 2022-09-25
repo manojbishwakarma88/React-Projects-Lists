ef4d8160cb3d5a0df1f27bd3810f6aab5ab775b9
manojbishwakarma <manojbishwakarma@neebal.com>
2022-09-25 23:33:55 +0530

    Birthday Lisr

---
+++ birthdaylist/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople]=useState(data);
+
+  return (
+  <main>
+    <section className='container'>
+  <h3 >{people.length} BirthDay Today</h3>
+  <List people={people}></List>
+  <button type='btn' onClick={()=>setPeople([])} > Clear All</button>
+  </section>
+  </main>
+  )
+}
+
+export default App;
